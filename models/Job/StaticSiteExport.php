08b1f27c7ec2f83be620a5375364243486e48e1b
Jim Safley <jimsafley@gmail.com>
2025-05-02 13:41:48 -0400

    Add taxonomy

--- models/Job/StaticSiteExport.php
+++ models/Job/StaticSiteExport.php
@@ -13,66 +13,16 @@ class Job_StaticSiteExport extends Omeka_Job_AbstractJob
     {
         try {
             $this->setStatus(Process::STATUS_IN_PROGRESS);
-
             $this->createSiteDirectory();
             $this->createItemsSection();
             // $this->createCollectionsSection();
             $this->createSiteArchive();
             $this->deleteSiteDirectory();
-
             $this->setStatus(Process::STATUS_COMPLETED);
-
         } catch (Exception $e) {
             $this->setStatus(Process::STATUS_ERROR);
             _log($e->getMessage(), Zend_Log::ERR);
         }
-    }
-
-    /**
-     * Create the items section.
-     */
-    public function createItemsSection()
-    {
-        $frontMatter = [
-            'title' => __('Items'),
-            'params' => [],
-        ];
-        $this->makeFile('content/items/_index.md', json_encode($frontMatter, JSON_PRETTY_PRINT | JSON_FORCE_OBJECT));
-
-        $page = 1;
-        do {
-            $items = get_db()->getTable('Item')->findBy([], 100, $page++);
-            foreach ($items as $item) {
-
-                $this->makeDirectory(sprintf('content/items/%s', $item->id));
-
-                $frontMatterPage = [
-                    'date' => (new DateTime(metadata($item, 'added')))->format('c'),
-                    'title' => metadata($item, 'display_title'),
-                    'draft' => $item->public ? false : true,
-                    'params' => [],
-                ];
-
-                // Build the markdown.
-                $markdown = '';
-                /*
-                 * @todo: Add the following to the markdown:
-                 *  - gallery of files (if configured)
-                 *  - element texts
-                 *  - links to files
-                 *  - link to collection
-                 *  - tags (hook into Hugo tags)
-                 *  - citation
-                 *  - content added by other plugins
-                 */
-
-                // Make the markdown file.
-                $this->makeFile(
-                    sprintf('content/items/%s/index.md', $item->id),
-                    sprintf("%s\n%s", json_encode($frontMatterPage, JSON_PRETTY_PRINT | JSON_FORCE_OBJECT), $markdown)
-                );
-            }
-        } while ($items);
     }
 
     /**
@@ -114,19 +64,119 @@ class Job_StaticSiteExport extends Omeka_Job_AbstractJob
             'baseURL' => $this->getStaticSite()->getDataValue('base_url'),
             'theme' => 'gohugo-theme-omeka-classic',
             'title' => get_option('site_title'),
-            // 'menus' => [
-            //     'main' => $menu->getArrayCopy(),
-            // ],
-            // 'params' => [
-            //     'homepage' => $homepage,
-            //     'theme' => $this->getStaticSite()->dataValue('theme'),
-            // ],
+            'sectionPagesMenu' => 'main',
             'pagination' => [
                 'pagerSize' => 25,
             ],
+            // 'params' => [],
         ]);
 
         $this->makeFile('hugo.json', json_encode($siteConfig->getArrayCopy(), JSON_PRETTY_PRINT));
+    }
+
+    /**
+     * Create the items section.
+     */
+    public function createItemsSection()
+    {
+        $frontMatter = [
+            'title' => __('Items'),
+            'params' => [],
+        ];
+        $this->makeFile('content/items/_index.md', json_encode($frontMatter, JSON_PRETTY_PRINT | JSON_FORCE_OBJECT));
+
+        $page = 1;
+        do {
+            $items = get_db()->getTable('Item')->findBy([], 100, $page++);
+            foreach ($items as $item) {
+
+                $this->makeDirectory(sprintf('content/items/%s', $item->id));
+                $this->makeDirectory(sprintf('content/items/%s/blocks', $item->id));
+
+
+                $frontMatterPage = new ArrayObject([
+                    'date' => (new DateTime(metadata($item, 'added')))->format('c'),
+                    'title' => metadata($item, 'display_title'),
+                    'draft' => $item->public ? false : true,
+                    'params' => [
+                        'itemID' => $item->id,
+                        'description' => metadata($item, array('Dublin Core', 'Description'), array('snippet' => 250)),
+                    ],
+                ]);
+
+                $blocks = $this->getItemPageBlocks($item, $frontMatterPage);
+                // @todo: Trigger an event
+                $this->makeBundleFiles(sprintf('items/%s', $item->id), $item, $frontMatterPage, $blocks);
+            }
+        } while ($items);
+    }
+
+    /**
+     * Get blocks for the passed item.
+     */
+    public function getItemPageBlocks($item, $frontMatterPage)
+    {
+        $blocks = new ArrayObject;
+
+        // Add item tags to page front matter.
+        $tags = [];
+        foreach ($item->Tags as $tag) {
+            $tags[] = $tag['name'];
+        }
+        $frontMatterPage['tags'] = $tags;
+
+        /*
+         * @todo: Add the following to the markdown:
+         *  - Gallery of files (if configured)
+         *  - Element texts
+         *  - Links to files
+         *  - Link to collection
+         *  - Tags (hook into Hugo tags)
+         *  - Citation : REQUIRES RE-IMPLEMENTATION OF Item::getCitation()
+         *  - Content added by other plugins
+         */
+
+        // $frontMatterBlock = new ArrayObject([
+        //     'params' => [
+        //         'id' => '',
+        //         'classes' => [''],
+        //     ],
+        // ]);
+        // $blockMarkdown = '';
+        // $blocks[] = [
+        //     'name' => '',
+        //     'frontMatter' => $frontMatterBlock,
+        //     'markdown' => $blockMarkdown,
+        // ];
+
+        return $blocks;
+    }
+
+    /**
+     * Make bundle files, including the index page and its block resources.
+     *
+     * Each block in $blocks must be an array containing the following elements:
+     *   - "name" (string): the unique block name
+     *   - "frontMatter" (ArrayObject): the block front matter
+     *   - "markdown" (string): the block markdown
+     */
+    public function makeBundleFiles($resourceContentPath, $resource, $frontMatterPage, $blocks)
+    {
+        // Make the block files.
+        $blockPosition = 0;
+        foreach ($blocks as $block) {
+            // Pad block numbers to get natural sorting for free.
+            $blockNumber = str_pad($blockPosition++, 4, '0', STR_PAD_LEFT);
+            $this->makeFile(
+                sprintf('content/%s/blocks/%s-%s.md', $resourceContentPath, $blockNumber, $block['name']),
+                sprintf("%s\n%s", json_encode($block['frontMatter'], JSON_PRETTY_PRINT), $block['markdown'])
+            );
+        }
+        // Make the markdown file.
+        $this->makeFile(
+            sprintf('content/items/%s/index.md', $resource->id),
+            json_encode($frontMatterPage, JSON_PRETTY_PRINT)
+        );
     }
 
     /**
